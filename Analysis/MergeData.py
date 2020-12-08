de71a62937d847e7d2c99158940a7ac83c2715d9
EJ-Chang <ola7984@gmail.com>
2020-12-08 15:14:10 +0800

    Data merged.

--- Analysis/MergeData.py
+++ Analysis/MergeData.py
@@ -9,12 +9,12 @@ import os
 from os import listdir
 from os.path import isfile, join
 
-fileMapping = {
+Dial_fileMapping = {
     'OSD' : {
         'Directory' : '/Users/YJC/Dropbox/ExpRecord_HSI/D_1',
         'MergeName' : 'OSD_D_Merge.txt'
         },
-    'OSD_Rev' : {
+    'REV' : {
         'Directory' : '/Users/YJC/Dropbox/ExpRecord_HSI/D_1_Rev',
         'MergeName' : 'OSD_D_Rev_Merge.txt'
         },
@@ -26,17 +26,45 @@ fileMapping = {
         'Directory' : '/Users/YJC/Dropbox/ExpRecord_HSI/D_RT',
         'MergeName' : 'RT_D_Merge.txt'
         }
-
 }
 
+Joystick_fileMapping = {
+    'OSD' : {
+        'Directory' : '/Users/YJC/Dropbox/ExpRecord_HSI/J_1',
+        'MergeName' : 'OSD_J_Merge.txt'
+        },
+    'REV' : {
+        'Directory' : '/Users/YJC/Dropbox/ExpRecord_HSI/J_1_Rev',
+        'MergeName' : 'OSD_J_Rev_Merge.txt'
+        },
+    'ACC' : {
+        'Directory' : '/Users/YJC/Dropbox/ExpRecord_HSI/J_ACC',
+        'MergeName' : 'ACC_J_Merge.txt'
+        },
+    'RT' : {
+        'Directory' : '/Users/YJC/Dropbox/ExpRecord_HSI/J_RT',
+        'MergeName' : 'RT_J_Merge.txt'
+        }
+}
 
 # Subject profile
 
-whichExp = input("Which experiment data do you want to merge? ").upper()
-print('OK, experiment %s is processing...' % whichExp)
+whichExp = input("Which experiment? (OSD/Rev/ACC/RT) ").upper()
+whichDevice = input("Which device? (J/D)").upper()
+print('OK, experiment %s of %s is processing...' % (whichExp, whichDevice))
 
 # Go to the choosen directory 
+if whichDevice == 'J':
+    fileMapping = Joystick_fileMapping
+
+elif whichDevice == 'D':
+    fileMapping = Dial_fileMapping
+
+else:
+    core.quit()
+
 os.chdir(fileMapping[whichExp]['Directory'])
+
 # Get file list
 fileList = os.listdir(fileMapping[whichExp]['Directory'])
 
@@ -58,7 +86,7 @@ for data in dataFiles:
 
     # Merge them
     for line in dataSet:
-        if 'NoMeaning' in line:
+        if 'None' in line:
             pass
         else:
             dataMerge.append([ID,line[:-1]])
